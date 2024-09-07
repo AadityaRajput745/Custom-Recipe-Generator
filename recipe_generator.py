938e5b86c02745da1a3e78b8923e9783c5c0da0d
Aaditya kumar <158083829+AadityaRajput745@users.noreply.github.com>
2024-09-07 17:39:08 +0000

    changes

--- recipe_generator.py
+++ recipe_generator.py
@@ -29,7 +29,7 @@ def hello():
  <!DOCTYPE html >
  <html >
  <head >
-  <title >Infinite Project Generator </title >
+  <title >Custom Recipe Generator </title >
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
     rel="stylesheet">
   <script >
